ed7144efb3e9ecd32f875e2e5a032469da15e07e
Anna Harbour <101281738+annaharbour@users.noreply.github.com>
2025-06-25 00:20:59 -0400

    order and orderlineitem models, orderdao, and order controller

---
+++ easyshop.api/src/main/java/org/yearup/models/Order.java
@@ -0,0 +1,102 @@
+package org.yearup.models;
+
+import java.math.BigDecimal;
+
+public class Order {
+    //order_id int AI PK
+    private int orderId;
+    //user_id int
+    private int userId;
+    //date datetime
+    private String date;
+    //address varchar(100)
+    private String address;
+    //city varchar(50)
+    private String city;
+    //state varchar(50)
+    private String state;
+    //zip varchar(20)
+    private String zip;
+    //shipping_amount decimal(10,2)
+    private BigDecimal shippingAmount;
+
+    public Order(int orderId, int userId, String date, String address, String city, String state, String zip,
+                 BigDecimal shippingAmount) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.date = date;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.shippingAmount = shippingAmount;
+    }
+
+    public Order() {
+
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public BigDecimal getShippingAmount() {
+        return shippingAmount;
+    }
+
+    public void setShippingAmount(BigDecimal shippingAmount) {
+        this.shippingAmount = shippingAmount;
+    }
+}
